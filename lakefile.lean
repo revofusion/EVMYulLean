b281c8a0edd4c6814b596126334a90b25ac169de
Revofusion <revofusionart@gmail.com>
2026-07-23 23:51:44 -0400

    build: update Lean and mathlib to v4.32.0

--- lakefile.lean
+++ lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 open Lake DSL System
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"@"v4.32.0-rc1"
+  "https://github.com/leanprover-community/mathlib4.git"@"v4.32.0"
 
 package «evmyul» {
   moreLeanArgs := #["-DautoImplicit=false"]
